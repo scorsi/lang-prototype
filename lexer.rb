226360954148fb7bbf28df6cb60b1eca8fe315f8
scorsi <sylvain.corsini@gmail.com>
2018-06-15 22:44:57 +0200

    Lexer finished

--- lexer.rb
+++ lexer.rb
@@ -1,51 +1,65 @@
 class Lexer
+  KEYWORDS = %w[def class if true false nil].freeze
   def tokenize(code)
     code.chomp!
     tokens = []
+    current_indent = 0
+    indent_stack = []
     i = 0
     while i < code.size
       chunk = code[i..-1]
-      puts chunk
-      puts tokens
       if identifier = chunk[/\A([a-z]\w*)/, 1]
-        tokens << [:identifier, identifier]
+        tokens << if KEYWORDS.include?(identifier)
+                    [identifier.to_sym, identifier]
+                  else
+                    [:identifier, identifier]
+                  end
         i += identifier.size
       elsif constant = chunk[/\A([A-Z]\w*)/, 1]
         tokens << [:constant, constant]
         i += constant.size
       elsif number = chunk[/\A([0-9]+)/, 1]
-        tokens << [:number, number]
+        tokens << [:number, number.to_i]
         i += number.size
       elsif string = chunk[/\A"([^"]*)"/, 1]
         tokens << [:string, string]
         i += string.size + 2
-      elsif declaration = chunk[/\A(\!)/, 1]
-        tokens << [:declaration, '!']
+      elsif indent = chunk[/\A\:\n( +)/m, 1]
+        if indent.size <= current_indent
+          raise "Bad indent level. Got #{indent.size} indents, " \
+                "expected > #{current_indent}"
+        end
+        current_indent = indent.size
+        indent_stack.push(current_indent)
+        tokens << [:indent, indent.size]
+        i += indent.size + 2
+      elsif indent = chunk[/\A\n( *)/m, 1]
+        if indent.size == current_indent
+          tokens << [:newline, '\n']
+        elsif indent.size < current_indent
+          while indent.size < current_indent
+            indent_stack.pop
+            current_indent = indent_stack.last || 0
+            tokens << [:dedent, indent.size]
+          end
+          tokens << [:newline, '\n']
+        else
+          raise "Missing ':'"
+        end
+        i += indent.size + 1
+      elsif operator = chunk[/\A(\|\||&&|==|!=|<=|>=)/, 1]
+        tokens << [operator, operator]
+        i += operator.size
+      elsif chunk =~ /\A /
         i += 1
-      elsif left_paren = chunk[/\A(\()/, 1]
-        tokens << [:left_paren, '(']
-        i += 1
-      elsif right_paren = chunk[/\A(\))/, 1]
-        tokens << [:right_paren, ')']
-        i += 1
-      elsif left_curly_bracket = chunk[/\A({)/, 1]
-        tokens << [:left_curly_bracket, '{']
-        i += 1
-      elsif right_curly_bracket = chunk[/\A(})/, 1]
-        tokens << [:right_curly_bracket, '}']
-        i += 1
-      elsif colon = chunk[/\A(:)/, 1]
-        tokens << [:colon, ':']
-        i += 1
-      elsif comma = chunk[/\A(,)/, 1]
-        tokens << [:comma, ',']
-        i += 1
-      elsif whitespace = chunk[/\A([ \t\n])+/, 1]
-        i += whitespace.size
       else
-        puts "Error at : \"#{chunk[0..10]}\""
-        return nil
+        value = chunk[0, 1]
+        tokens << [value, value]
+        i += 1
       end
+    end
+    while indent = indent_stack.pop
+      tokens << [:dedent, indent_stack.first || 0]
     end
     tokens
   end
